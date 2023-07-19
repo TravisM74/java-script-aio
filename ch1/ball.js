08ce10ce9a4c57d71a608d6b877a6d536e02d03b
Travis Mouna <travis.mouna@gmail.com>
2023-07-19 15:07:02 +0300

    first commit

---
+++ ch1/ball.js
@@ -0,0 +1,26 @@
+const ball = document.getElementById('ball');
+document.addEventListener('keydown', handleKeyPress);
+let position = 0;
+let yPosition =0;
+function handleKeyPress(e) {
+ if (e.code === 'ArrowLeft') {
+ position = position - 10;
+ }
+ if (e.code === 'ArrowRight') {
+ position = position + 10;
+ }
+ if (e.code === 'ArrowDown') {
+    yPosition = yPosition + 10;
+    }
+    if (e.code === 'ArrowUp') {
+        yPosition = yPosition - 10;
+        }
+ if (position < 0) {
+ position = 0;
+ }
+ refresh();
+}
+function refresh() {
+ ball.style.left = position + 'px';
+ ball.style.top = yPosition + 'px';
+}
---
+++ ch1/ball.md
@@ -0,0 +1,14 @@
+# Gamelet
+
+stater script for writing javascript games.
+
+## Usage
+
+1. include ball.js in the html document containing an element with an id of 'ball'
+
+```html
+<div id="ball">@</div>
+<script src="ball.js" />
+```
+
+2. the script will detect when an arrow key is pressed and move the ball accordingly.
